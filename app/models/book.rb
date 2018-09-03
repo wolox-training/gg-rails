b6c3a671a5df7f7192b1b938e951907c3ab7fabd
Leonel Gutierrez <gonzalo.gutierrez@wolox.com.ar>
2018-09-03 15:30:51 -0300

    Removed some references in user and book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,3 @@
 class Book < ApplicationRecord
   validates :genre, :author, :image, :title, :publisher, :year, presence: true
-
-  has_many :rent, dependent: :nullify
-  has_many :user, through: :rent
 end
