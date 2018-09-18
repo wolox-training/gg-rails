9bc8678e6af5db413106e143056fc95293c3d532
Leonel Gutierrez <gonzalo.gutierrez@wolox.com.ar>
2018-09-18 13:32:00 -0300

    Added create rent test

--- spec/controllers/api/v1/rents_controller_spec.rb
+++ spec/controllers/api/v1/rents_controller_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 describe Api::V1::RentsController do
   include_context 'Authenticated User'
 
+  let!(:book) { create(:book) }
+  let!(:other_user) { create(:user) }
+
   describe 'GET #index' do
-    let!(:book) { create(:book) }
-    let!(:another_user) { create(:user) }
     let!(:rents) { create_list(:rent, 3, user: user, book: book) }
+    let!(:other_rents) { create_list(:rent, 3, user: other_user, book: book) }
 
     context 'When fetching all rents from the user' do
       before do
@@ -19,6 +21,37 @@ describe Api::V1::RentsController do
 
       it 'responds with 200 status' do
         expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    let(:from) { Faker::Date.backward(3) }
+    let(:to) { Faker::Date.forward(3) }
+    let(:rent_params) do
+      { 'user_id' => user.id, 'book_id' => book.id, 'from' => from, 'to' => to }
+    end
+    let(:other_rent_params) do
+      { 'user_id' => other_user.id, 'book_id' => book.id, 'from' => from, 'to' => to }
+    end
+
+    context 'When create a rent' do
+      before do
+        post :create, params: { user_id: user.id, rent: rent_params }
+      end
+
+      it 'responds with 201 status' do
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'When create a rent with invalid params' do
+      before do
+        post :create, params: { user_id: user.id, rent: other_rent_params }
+      end
+
+      it 'responds with 401 status' do
+        expect(response).to have_http_status(:unauthorized)
       end
     end
   end
