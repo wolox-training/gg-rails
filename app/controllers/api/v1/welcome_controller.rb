e7d0aa1e0d11c7f712ec3d44ba0a4b409a1940cf
Leonel Gutierrez <gonzalo.gutierrez@wolox.com.ar>
2018-09-19 16:43:23 -0300

    Added locale to welcome view

--- app/controllers/api/v1/welcome_controller.rb
+++ app/controllers/api/v1/welcome_controller.rb
@@ -1,8 +1,18 @@
 module Api
   module V1
     class WelcomeController < ApplicationController
+      before_action :config_locale, only: [:index]
+
       def index
         @user_full_name = current_user.first_name + current_user.last_name if current_user
+      end
+
+      def config_locale
+        I18n.locale = if current_user
+                        current_user.locale || I18n.default_locale
+                      else
+                        I18n.default_locale
+                      end
       end
     end
   end
