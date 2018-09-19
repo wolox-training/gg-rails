ae6045d25b6cb45127826e6de6d7b0f9515e59be
Leonel Gutierrez <gonzalo.gutierrez@wolox.com.ar>
2018-09-19 13:41:54 -0300

    Fixed some rubocop styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,12 +18,12 @@ class User < ApplicationRecord
 
   def self.from_omniauth(access_token)
     data = access_token.info
-    user = User.where(email: data['email']).first
+    user = User.find_by(email: data['email'])
 
-    # Uncomment the section below if you want users to be created if they don't exist
-    unless user
-      user = User.create(first_name: data['first_name'], last_name: data['last_name'], email: data['email'], password: Devise.friendly_token[0, 20])
-    end
+    user ||= User.create(
+      first_name: data['first_name'], last_name: data['last_name'],
+      email: data['email'], password: Devise.friendly_token[0, 20]
+    )
     user
   end
 end
