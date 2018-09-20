30767f7b40f9da70f096c17d67892c91bacf2a8f
Leonel Gutierrez <gonzalo.gutierrez@wolox.com.ar>
2018-09-20 15:04:25 -0300

    Move some code from welcome to application controller

--- app/controllers/api/v1/welcome_controller.rb
+++ app/controllers/api/v1/welcome_controller.rb
@@ -1,8 +1,6 @@
 module Api
   module V1
     class WelcomeController < ApplicationController
-      before_action :config_locale, only: [:index]
-
       def index
         @user_full_name = current_user.first_name + current_user.last_name if current_user
       end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  before_action :config_locale
 
   def config_locale
     I18n.locale = current_user ? current_user.locale || I18n.default_locale : I18n.default_locale
